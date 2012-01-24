ac887c85fdbf61cd00e4f846135ae524a1479252
Steven Merrill <steven.merrill@gmail.com>
2012-01-24 14:26:18 -0500

    Add a function to invoke PHP_Codesniffer by hand.

--- index.php
+++ index.php
@@ -5,6 +5,41 @@ require 'vendor/.composer/autoload.php';
 
 use Guzzle\Http\Client;
 
+/**
+ * Runs PHP_Codesniffer as though you had done the following on the command line:
+ * phpcs --standard=DrupalCodingStandard --extensions=php,module,inc,install,test,profile,theme --report=xml $file_path
+ *
+ */
+function run_php_codesniffer($file_path) {
+  if (is_file(dirname(__FILE__).'/../CodeSniffer/CLI.php') === true) {
+    include_once dirname(__FILE__).'/../CodeSniffer/CLI.php';
+  } else {
+    include_once 'PHP/CodeSniffer/CLI.php';
+  }
+
+  $phpcs = new PHP_CodeSniffer_CLI();
+  $phpcs->checkRequirements();
+
+  # Fake some command-line values.
+  $_SERVER['argc'] = 0;
+  $_SERVER['argv'] = array();
+
+  # Set up our own set of values by hand.
+  $config_values = $phpcs->getDefaults();
+  $config_values['extensions'] = array('php', 'module', 'inc', 'install', 'test', 'profile', 'theme');
+  $config_values['files'] = array($file_path);
+  $config_values['standard'] = 'DrupalCodingStandard';
+  $config_values['reports'] = array('xml' => null);
+
+  // Use output buffering to grab the XML output.
+  ob_start();
+  $numErrors = $phpcs->process($config_values);
+  $xml_output = ob_get_contents();
+  ob_end_clean();
+
+  return $xml_output;
+}
+
 # @TODO: Get these values from a GitHub hook.
 $user = 'smerrill';
 $repo = 'cod_api';
@@ -30,7 +65,7 @@ $pull_request_info = json_decode($pull_request_response->getBody());
 
 # Iterate over the list of files. For each file:
 #   1. Parse the diff markers to know which diff positions are visible and can have comments posted to them.
-#   2. Run `phpcs --standard=./drupalcs/DrupalCodingStandard --extensions=php,module,inc,install,test,profile,theme -n --report=xml $FILE`
+#   2. Download the files and run them through run_php_codesniffer().
 #   3. Use SimpleXML to iterate over the CodeSniffer XML and grab all errors that are within the visible diff positions.
 #   4. Post those as comments through the GitHub Pull Request Comments API.
 
